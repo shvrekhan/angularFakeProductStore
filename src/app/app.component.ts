8cf38f5d04c433ea62d3f242002504862f29e229
shvrekhan <shvrekhan@gmail.com>
2023-02-14 18:35:14 +0530

    displayed all products and empty cart function completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import product from './services/interfaces';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'fake-api-store';
