05efb7c3242d4a5314d4fd564f8609a180601ade
shvrekhan <shvrekhan@gmail.com>
2023-02-14 18:36:23 +0530

    button for view product added

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,10 +1,30 @@
 import { Component, Input } from '@angular/core';
+import product from '../services/interfaces';
+import { ApiServiceService } from '../services/api-service.service';
 
+import { Router } from '@angular/router';
+import { CartService } from '../services/cart-service';
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.css'],
 })
 export class ProductCardComponent {
-  @Input() productList: any;
+  public productList: any;
+  constructor(
+    private apiService: ApiServiceService,
+    private router: Router,
+    private cartService: CartService
+  ) {
+    apiService.getProducts().subscribe((productsData) => {
+      console.log(productsData);
+      this.productList = productsData;
+    });
+  }
+  onSelect(product: product) {
+    this.router.navigate(['product', product.id, product]);
+  }
+  addtocart(product: product) {
+    this.cartService.addtoCart(product);
+  }
 }
