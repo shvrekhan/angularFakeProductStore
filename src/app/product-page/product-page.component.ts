e30d87b5fb5a502c8dd2cde537e8957b79be57d6
shvrekhan <shvrekhan@gmail.com>
2023-02-14 18:37:04 +0530

    display single product on page

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import product from '../services/interfaces';
 @Component({
   selector: 'app-product-page',
   templateUrl: './product-page.component.html',
-  styleUrls: ['./product-page.component.css']
+  styleUrls: ['./product-page.component.css'],
 })
-export class ProductPageComponent {
-
+export class ProductPageComponent implements OnInit {
+  public productId: any;
+  public product: any;
+  public rating: any;
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit() {
+    let id = Number(this.route.snapshot.paramMap.get('id'));
+    this.rating = this.route.snapshot.paramMap.get('rating');
+    this.productId = id;
+    console.log(this.route.snapshot.params['category']);
+    this.product = this.route.snapshot.params;
+    console.log(this.rating);
+  }
 }
